e2acb8910e33bc29b0561effb9e70e059bbe5e56
Menachem35 <menachemglik@yahoo.com>
2017-06-12 11:43:48 -0400

    Create page-qa.php

---
+++ page-qa.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: QA */ ?>
+
+<?php get_header(); ?>
+
+	<div class="row" id="inner_page_bg">			
+		
+		<div class="inner_box_QA_Right">		
+		
+			
+				
+		</div>
+
+		<div class="inner_box_QA_left">		
+		
+			
+				
+		</div>
+		
+	</div><!--row -->
+			
+	
+<?php get_footer(); ?>
